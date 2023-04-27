985528d7d7082eeddc6a784b49fa7beff09f8eb9
Robbie <SmithR69@cardiff.ac.uk>
2023-04-27 21:55:03 +0100

    Updated run.py - Database already created

--- run.py
+++ run.py
@@ -4,6 +4,4 @@ from shop import db
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-        with app.app_context():
-                db.create_all()
-app.run(debug=True)
+        app.run(debug=True)
