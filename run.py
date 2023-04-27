1b91bbbc75558095e3d2de8e978004d43b10327a
Robbie <SmithR69@cardiff.ac.uk>
2023-04-27 21:48:10 +0100

    Database Infrastructure complete

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from shop import app
+from shop import db
+
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-        app.run(debug=True)
+        with app.app_context():
+                db.create_all()
+app.run(debug=True)
--- shop/models.py
+++ shop/models.py
@@ -1,20 +1,12 @@
 from shop import db
 
-class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(length=30), nullable=False , unique=True)
-    email_address = db.Column(db.String(length=60), nullable = False, unique = True)
-    password_hash = db.Column(db.String(length=60), nullable = False, unique = True)
-    budget = db.Column(db.Integer(), nullable = False, default = 1000)
-    items = db.relationship('Item', backref='owned_user', lazy=True)
 
 class Item(db.Model): #Telling flask this class is a model
     id = db.Column(db.Integer(), primary_key = True) #Creating ID as primary keys
     name = db.Column(db.String(length=30), nullable = False, unique = True ) #Can only have max 30 characters, cannot be empty, must be a unique field name
     price = db.Column(db.Integer(), nullable = False)
-    barcode = db.Column(db.String(length=12), nullable = False, unique = True)
     description = db.Column(db.String(length = 1024), nullable = False, unique = True)
-    owner = db.Column(db.Integer(), db.ForeignKey('user.id') )
+    environmental_impact = db.Column(db.Integer(), nullable = False)
 
     def __repr__(self):
         return f'Item {self.name}'
